5b9619ca84178bafa8844bbb4d236d0bacda0494
km.kirillov <km.kirillov@tensor.ru>
2023-10-18 16:48:31 +0300

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,28 @@
 export function romanToInteger(str) {
-    let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let result = 0,
+        objRomanInteger = {
+            I: 1,
+            V: 5,
+            X: 10,
+            L: 50,
+            C: 100,
+            D: 500,
+            M: 1000,
+        },
+        flag = false;
+    for (let i = str.length - 1; i >= 0; i--) {
+        let currElem = objRomanInteger[str[i]],
+            nextElem = objRomanInteger[str[i - 1]];
+        if (flag) {
+            flag = false;
+            continue;
+        } else if (currElem > nextElem) {
+            flag = true;
+            result += currElem - nextElem;
+        } else {
+            result += currElem;
+            flag = false;
+        }
+    }
     return result;
 }
